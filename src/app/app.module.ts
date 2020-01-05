a82264c5fd7a1f1f2a8fc755db7cfece7948308b
wakeofgod <232028373@qq.com>
2020-01-06 00:14:18 +0800

    coomit code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
-
+import {TodolistComponent} from 'src/app/components/todolist/todolist.component';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    TodolistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/todolist/todolist.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+  public keyword:string;
+  public todolist:any[]=[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+  doAdd(event:any){
+    debugger;
+    if(event.keyCode==13){
+      if(!this.todolistHasKeyword(this.todolist,this.keyword)){
+        this.todolist.push({
+          title:this.keyword,
+          status:0
+        });
+      }
+      else{
+        alert(`${this.keyword} have exist`);
+      }  
+    }
+
+  }
+  deleteData(key:any){
+    this.todolist.splice(key,1);
+  }
+  todolistHasKeyword(todolist:any,keyword:string):boolean{
+    //#region 异步可能会出错
+    // this.todolist.forEach(item=>{
+    //   if(item.title==keyword){
+    //     return true;
+    //   }
+    // });
+    // return false;
+    //#endregion
+    for(let i:number=0;i<todolist.length;i++){
+      if(todolist[i].title==keyword){
+        return true;
+      }
+    }
+    return false;
+  }
+}
